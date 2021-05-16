1c551fac34518ebb878f41e59d68742156b12627
Teselea Ggheorghita <ghteselea@gmail.com>
2021-05-16 16:53:03 +0300

    added cache for the image download UIImageView extension

--- FoodApp/Utils/Extensions/UIImageView+Extension.swift
+++ FoodApp/Utils/Extensions/UIImageView+Extension.swift
@@ -9,9 +9,9 @@ import Foundation
 import UIKit
 
 extension UIImageView {
-    func load(url: String) {
+    func loadWithoutCache(url: String) {
         guard let url = URL(string: url) else {
-            print("Nu s-a putut incarca imaginea")
+            print("URL is broken")
             return
         }
         DispatchQueue.global().async { [weak self] in
@@ -24,4 +24,30 @@ extension UIImageView {
             }
         }
     }
+    
+    func load(url: String, placeholder: UIImage? = UIImage(named: "no_image"), cache: URLCache? = nil) {
+        
+        guard let url = URL(string: url) else {
+            print("URL is broken")
+            return
+        }
+        
+        let cache = cache ?? URLCache.shared
+        let request = URLRequest(url: url)
+        if let data = cache.cachedResponse(for: request)?.data, let image = UIImage(data: data) {
+            self.image = image
+        } else {
+            self.image = placeholder
+            URLSession.shared.dataTask(with: request, completionHandler: { (data, response, error) in
+                if let data = data, let response = response, ((response as? HTTPURLResponse)?.statusCode ?? 500) < 300, let image = UIImage(data: data) {
+                    let cachedData = CachedURLResponse(response: response, data: data)
+                    cache.storeCachedResponse(cachedData, for: request)
+                    DispatchQueue.main.async() {
+                        self.image = image
+                    }
+                }
+            }).resume()
+        }
+    }
 }
+
